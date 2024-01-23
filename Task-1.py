22021274eb4889eba2d38c4195d72222a4d78c85
Fathik <mrfattan08@gmail.com>
2024-01-23 16:22:46 +0530

    add

---
+++ Task-1.py
@@ -0,0 +1,10 @@
+# Get user input for variable 'a'
+a = int(input("Enter the value of a: "))
+
+# Check conditions and print statements accordingly
+if a > 60:
+    print("I like Apple")
+elif a > 30:
+    print("I like Chocolate")
+elif a > 20:
+    print("I like Ice Cream")
---
+++ Task-2.py
@@ -0,0 +1,4 @@
+year = 2024
+leap_year = (year % 400 == 0)
+
+print(leap_year)
